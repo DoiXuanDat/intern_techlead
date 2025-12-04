f356847eb8d5dbbace69e50047676fde72877a12
xuandat <doixuandat1@gmail.com>
2025-12-04 15:05:28 +0700

    day2

--- day2/test.py
+++ day2/test.py
@@ -9,26 +9,7 @@ def traverseAndPrint(head):
         print(currentNode.data, end=" -> ")
         currentNode = currentNode.next
     print("null")
-
-def sortLinkedList(head):
-    current = head
-    index = None
-
-    if head is None:
-        return
     
-    while current is not None:
-        index = current.next
-        
-        while index is not None:
-            if current.data > index.data:
-                temp = current.data
-                current.data = index.data
-                index.data = temp
-            
-            index = index.next
-        current = current.next
-
 node1 = Node(7)
 node2 = Node(11)
 node3 = Node(3)
@@ -38,12 +19,6 @@ node5 = Node(9)
 node1.next = node2
 node2.next = node3
 node3.next = node4
+
 node4.next = node5
-
-print("Trước khi sắp xếp:")
-traverseAndPrint(node1)
-
-sortLinkedList(node1)
-
-print("Sau khi sắp xếp:")
-traverseAndPrint(node1)
+print(traverseAndPrint(node1))
